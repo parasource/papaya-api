978be226d2da5617f6dfe70f766f3256d1517ae8
sireax <sireax@yandex.ru>
2022-12-31 17:22:55 +0400

    fix feed panic

--- api/v1/handlers/feed.go
+++ api/v1/handlers/feed.go
@@ -68,14 +68,16 @@ func HandleFeed(c *gin.Context) {
 		c.AbortWithStatus(500)
 		return
 	}
-	var todayLookItems []*models.WardrobeItem
-	err = database.DB().Raw("SELECT * FROM wardrobe_items JOIN look_items li on wardrobe_items.id = li.wardrobe_item_id WHERE li.look_id = ?", todayLook.ID).Find(&todayLookItems).Error
-	if err != nil {
-		logrus.Errorf("error getting today's looks items: %v", err)
-		c.AbortWithStatus(500)
-		return
+	if todayLook != nil {
+		var todayLookItems []*models.WardrobeItem
+		err = database.DB().Raw("SELECT * FROM wardrobe_items JOIN look_items li on wardrobe_items.id = li.wardrobe_item_id WHERE li.look_id = ?", todayLook.ID).Find(&todayLookItems).Error
+		if err != nil {
+			logrus.Errorf("error getting today's looks items: %v", err)
+			c.AbortWithStatus(500)
+			return
+		}
+		todayLook.Items = todayLookItems
 	}
-	todayLook.Items = todayLookItems
 
 	// Categories
 	var categories []models.Category
